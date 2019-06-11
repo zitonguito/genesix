a40480d9a92840a1d0efbecd0ade1cbd698fd4ba
zitonguito <zitonguito@gmail.com>
2019-06-11 01:14:04 -0500

    Se agrego <#!/bin/bash> a la primero linea

--- SREPMOVDPT.sh
+++ SREPMOVDPT.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 ###################################################################################
 # Script   : SREPMOVDPT.sh                                                        #
 # Objetivo : Agregar cabecera correspondiente y enviar via correo                 #
